86e1d73a372b2e2fc71ca9c258abe3fcccf1ffff
SyrgakRavshanbekUulu <75747709+SyrgakRavshanbekUulu@users.noreply.github.com>
2021-07-02 15:21:49 +0600

    finish base framework functional (#3)
    Co-authored-by: sravshanbek <sravshanbek@it.gov.kg>

---
+++ src/core/DomListener.js
@@ -0,0 +1,34 @@
+import { capitalize } from "./utils"
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getEventName(listener)
+      if (!this[method]) {
+        throw new
+        Error(`Method ${method} is not implemented in ${this.name} Component`)
+      }
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getEventName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getEventName(eventName) {
+  return 'on' + capitalize(eventName)
+}
