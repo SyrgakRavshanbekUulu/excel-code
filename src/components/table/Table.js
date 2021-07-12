cbaf2a1c1ba3d995e09246a3c8d66e49ed2dab12
sravshanbek <sravshanbek@it.gov.kg>
2021-07-13 03:14:44 +0600

    finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,24 @@
 import {ExcelComponent} from "../../core/ExcelComponent";
-import { createTable } from "./table.templete";
+import {createTable} from "./table.templete";
+import {resizeHundler} from "./table.resize";
+import {shouldResize} from "./tabke.functions";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
+  }
+
   toHtml() {
     return createTable()
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHundler(this.$root, event)
+    }
   }
 }
