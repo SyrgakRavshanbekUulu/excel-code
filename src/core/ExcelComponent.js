ffd57db7380b0d38056df1cc5ae36d56423d1bd2
sravshanbek <sravshanbek@it.gov.kg>
2021-07-15 17:24:03 +0600

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,15 +4,40 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
+    this.prepare()
   }
+  // Настраиваем наш компонент до init
+  prepare() {
+
+  }
+  // Возвращает шаблон компонента
   toHtml() {
     return ''
   }
 
+  // Уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписываемся на сабытие event
+  $on(event, fn) {
+    const unsubs = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsubs)
+  }
+  // инициализируем компонент
+  // добавлаем ДОМ слушателей
   init() {
     this.initDOMListeners()
   }
+
+  // Удаляем компонент
+  // Чистим слушатели
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
--- src/core/utils.js
+++ src/core/utils.js
@@ -4,3 +4,12 @@ export function capitalize(str) {
   }
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export function range(start, end) {
+  if (start > end) {
+    [start, end] = [end, start]
+  }
+  return new Array(end - start + 1)
+      .fill('')
+      .map((_, index) => start + index)
+}
